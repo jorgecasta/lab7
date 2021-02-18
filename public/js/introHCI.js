897bcf8fd87e0607ce6acda9471a6588bd832e4a
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-02-18 14:07:40 -0800

    alternate view

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,14 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(recordClick);
+}
+
+function recordClick(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	ga("send", "event", 'like', 'click');
+
+	console.log("User clicked on like button");
 }
